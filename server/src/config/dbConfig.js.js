d0d542b3a453907242296612f09c4bd2a3193451
Tinwana <125630233+Tinwana@users.noreply.github.com>
2023-11-21 17:40:22 +0700

    ao

--- server/src/config/dbConfig.js.js
+++ server/src/config/dbConfig.js.js
@@ -1,15 +1,9 @@
 import mongoose from "mongoose";
-const mongooseOptions = {
-  dbName: "auth", // Replace with your actual database name
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+
 async function connectDb() {
   try {
     const url = process.env.DATABASE_URL;
-    const trueUrl = url.toString();
-    console.log(trueUrl);
-    await mongoose.connect(trueUrl, { dbName: "auth" });
+    await mongoose.connect(url, { dbName: "auth" });
     console.log("connect successful!");
   } catch (err) {
     console.log("connect failure!", err);
